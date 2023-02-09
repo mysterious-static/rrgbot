bde8edb9daf14bcc7d570099452ba1a0c992776c
aletson <me@ajl.io>
2023-02-08 19:52:05 -0500

    asdf

--- index.js
+++ index.js
@@ -499,7 +499,7 @@ client.on('interactionCreate', async (interaction) => {
                 collector.on('collect', async (interaction_second) => {
                     if (interaction_second.customId == 'ArchetypeAssignmentSelector') {
                         selectedArchetype = interaction_second.values[0];
-                        var characters = await connection.promise().query('select * from characters left outer join characters_archetypes ca on characters.id = ca.character_id where guild_id = ? and ca.archetype_id <> ?', [interaction.guildId, selectedArchetype]);
+                        var characters = await connection.promise().query('select * from characters left outer join characters_archetypes ca on characters.id = ca.character_id where guild_id = ? and (ca.archetype_id <> ? or ca.archetype_id is null)', [interaction.guildId, selectedArchetype]);
                         if (characters[0].length > 0) {
                             var charactersKeyValues = [];
                             for (const character of characters[0]) {
