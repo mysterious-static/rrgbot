f609ba4493bd8a3197b7c0663307ae867fd32cbf
aletson <me@ajl.io>
2023-03-12 19:39:06 -0400

    asdf

--- index.js
+++ index.js
@@ -867,11 +867,11 @@ client.on('interactionCreate', async (interaction) => {
                                 if (charactersSelected) {
                                     console.log(charactersSelected);
                                     for (const character_id of charactersSelected) {
-                                        await connection.promise().query('insert into skills_characters (character_id, skill_id) values (?, ?)', [character_id, skillSelected]);
+                                        await connection.promise().query('insert ignore into skills_characters (character_id, skill_id) values (?, ?)', [character_id, skillSelected]);
                                     }
                                 } else {
                                     for (const archetype_id of archetypesSelected) {
-                                        await connection.promise().query('insert into skills_archetypes (archetype_id, skill_id) values (?, ?)', [archetype_id, skillSelected]);
+                                        await connection.promise().query('insert ignore into skills_archetypes (archetype_id, skill_id) values (?, ?)', [archetype_id, skillSelected]);
                                     }
                                 }
                                 await interaction_second.update({ content: 'Successfully assigned skill to characters or archetypes. I\'d tell you which but Alli is lazy.', components: [] });
