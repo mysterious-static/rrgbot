a9fc0499aed7445a0196b445073b2981eb10e169
aletson <me@ajl.io>
2023-03-15 16:45:12 -0400

    asdf

--- index.js
+++ index.js
@@ -378,6 +378,7 @@ client.on('interactionCreate', async (interaction) => {
             var locations = await connection.promise().query('select * from movement_locations where guild_id = ?', [interaction.guildId]);
             var players = await connection.promise().query('select p.user_id, c.location_id from players p join players_characters pc on p.id = pc.player_id join characters c on c.id = pc.character_id where pc.active = 1');
             if (locations[0].length > 0) {
+                interaction.deferUpdate();
                 for (const thisLocation of locations[0]) {
                     var channel = await client.channels.cache.get(thisLocation.channel_id);
                     if (thisLocation.global_read == true) {
@@ -406,6 +407,7 @@ client.on('interactionCreate', async (interaction) => {
                         // get all players, for each player if active character is in location then set read and send true else false.
                     }
                 }
+                interaction.reply({content: 'Reset done.', ephemeral: true});
             }
         } else if (interaction.commandName == 'locationvisibility') {
             var thisChannel = await interaction.options.getChannel('location');
