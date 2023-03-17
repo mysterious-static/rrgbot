41ddd3a172cea5a90a0f7a6976022af42e47f076
aletson <me@ajl.io>
2023-03-17 15:50:13 -0400

    asfd

--- index.js
+++ index.js
@@ -1585,6 +1585,50 @@ client.on('interactionCreate', async (interaction) => {
                                 innateSelected = interaction_second.values[0];
                                 await connection.promise().query('insert into duels_innates (duel_id, character_id, skill_id) values (?, ?, ?)', [duel_id, activeCharacter.id, innateSelected]);
                                 interaction_second.update({ content: "Innate selected.", components: [] });
+                                // BEGIN DUEL REDRAW BLOCK
+                                var results = await connection.promise().query('select * from duels where id = ?', duel_id);
+                                var duel = results[0][0];
+                                var healthStat = await connection.promise().query('select * from stats join stats_specialstats sps on stats.id = sps.stat_id where stats.guild_id = ? and sps.special_type = "health"', [interaction.guildId]);
+                                var target = interaction.options.getUser('target');
+                                var player = await connection.promise().query('select c.* from characters c where character_id = ?', [duel.player_id]);
+                                var target = await connection.promise().query('select c.* from characters c where character_id = ?', [duel.target_id]);
+                                var isCustomPlayerHealth = await connection.promise().query('select override_value from characters_stats where character_id = ? and stat_id = ?', [player[0][0].id, isHealthStat[0][0].id]);
+                                var isCustomTargetHealth = await connection.promise().query('select override_value from characters_stats where character_id = ? and stat_id = ?', [target[0][0].id, isHealthStat[0][0].id]);
+                                var results = await connection.promise().query('select * from duels_rounds where duel_id = ? order by id desc limit 1', [duel_id]);
+                                if (results[0].length > 0) {
+                                    var currentRound = results[0][0];
+                                    if (currentRound.winner_id) {
+                                        var displayRound = currentRound.round_id + 1;
+                                    } else {
+                                        var displayRound = currentRound.round_id;
+                                    }
+                                } else {
+                                    var displayRound = 1;
+                                }
+                                if (isCustomPlayerHealth[0].length > 0) {
+                                    var computedPlayerHealth = isCustomPlayerHealth[0][0].override_value;
+                                } else {
+                                    var computedPlayerHealth = healthStat[0][0].default_value;
+                                }
+                                if (isCustomTargetHealth[0].length > 0) {
+                                    var computedTargetHealth = isCustomTargetHealth[0][0].override_value;
+                                } else {
+                                    var computedTargetHealth = healthStat[0][0].default_value;
+                                }
+                                var embed = new EmbedBuilder()
+                                    .setTitle(`DUEL: ${player[0][0].name} v. ${target[0][0].name}`)
+                                    .setDescription(`Round ${displayRound}`)
+                                    .addFields(
+                                        { name: player[0][0].name, value: `${healthStat[0][0].name}: ${computedPlayerHealth}`, inline: true }, // active skills, innates, etc
+                                        { name: target[0][0].name, value: `${healthStat[0][0].name}: ${computedTargetHealth}`, inline: true } // active skills, innates, etc
+                                    );
+                                var duelButtonR = new ButtonBuilder().setCustomId('duelButtonR' + duel[0].insertId).setLabel('Rapid').setStyle('Primary'); // TODO ButtonBuilder doesn't exist in Discord.js v14
+                                var duelButtonP = new ButtonBuilder().setCustomId('duelButtonP' + duel[0].insertId).setLabel('Precision').setStyle('Primary');
+                                var duelButtonS = new ButtonBuilder().setCustomId('duelButtonS' + duel[0].insertId).setLabel('Sweeping').setStyle('Primary');
+                                var duelButtonSkill = new ButtonBuilder().setCustomId('duelButtonSkill' + duel[0].insertId).setLabel('Declare Innates').setStyle('Primary');
+                                const rpsRow = new ActionRowBuilder().addComponents(duelButtonR, duelButtonP, duelButtonS, duelButtonSkill);
+                                await interaction.message.edit({ embeds: [embed], components: [rpsRow] });
+                                // END DUEL REDRAW BLOCK
                                 await collector.stop();
                             });
                         } else {
