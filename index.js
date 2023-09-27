a2af0a4ba0055b5007350d603625c571b9f246a6
aletson <me@ajl.io>
2023-09-27 14:09:33 -0400

    sdf

--- index.js
+++ index.js
@@ -1528,8 +1528,11 @@ client.on('interactionCreate', async (interaction) => {
                                     characterSelected = interaction_second.values[0];
                                 }
                                 if (statSelected && characterSelected) {
+                                    console.log(statSelected);
+                                    console.log(characterSelected);
                                     var exists = await connection.promise().query('select * from characters_stats where stat_id = ? and character_id = ?', [statSelected, characterSelected]);
                                     if (exists[0] && exists[0].length > 0) {
+                                        console.log('exists');
                                         await connection.promise().query('update characters_stats set override_value = ? where character_id = ? and stat_id = ?', [value, statSelected, characterSelected]);
                                     } else {
                                         await connection.promise().query('insert into characters_stats (character_id, stat_id, override_value) values (?, ?, ?)', [characterSelected, statSelected, value]);
