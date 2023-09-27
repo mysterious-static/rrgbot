23b2f7901133e7a15d59984b7fa5a24922180010
aletson <me@ajl.io>
2023-09-27 12:10:29 -0400

    fix missing argument

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function process_effect(character, effect, source, guildId, target = null)
                 var effects = await connection.promise().query('select e.* from effects e join reputations_tiers_effects rte on e.id = rte.effect_id join reputations_tiers rt on rt.id = rte.reputationtier_id where rt.value > ? and rt.value <= ? and rt.reputation_id = ?', [old_value, old_value + effect.type_qty, effect.type_id]);
                 if (effects[0].length > 0) {
                     for (const thisEffect of effects[0]) {
-                        await process_effect(character, thisEffect, guildId, 'reputationtier');
+                        await process_effect(character, thisEffect, reputation[0][0].id, guildId, 'reputationtier');
                     }
                 }
                 break;
@@ -180,7 +180,7 @@ async function process_effect(character, effect, source, guildId, target = null)
                 var effects = await connection.promise().query('select e.* from effects e join reputations_tiers_effects rte on e.id = rte.effect_id join reputations_tiers rt on rt.id = rte.reputationtier_id where rt.value > ? and rt.value <= ? and rt.reputation_id = ?', [old_value, old_value + effect.type_qty, effect.type_id]);
                 if (effects[0].length > 0) {
                     for (const thisEffect of effects[0]) {
-                        await process_effect(character, thisEffect, guildId, 'reputationtier');
+                        await process_effect(character, thisEffect, reputation[0][0].id, guildId, 'reputationtier');
                     }
                 }
                 break;
