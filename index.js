e3a3eef311fbf98388545b57cb92727d5c89cb45
aletson <me@ajl.io>
2023-09-29 21:12:00 -0400

    ===

--- index.js
+++ index.js
@@ -3929,7 +3929,8 @@ client.on('interactionCreate', async (interaction) => {
                             collector.on('collect', async (interaction_second) => {
                                 if (interaction_second.member.id === interaction.member.id) {
                                     if (interaction_second.values[0]) {
-                                        let skillSelected = skills[0].find(s => s.id === interaction_second.values[0]);
+                                        let skillSelected = skills[0].find(s => s.id == interaction_second.values[0]);
+                                        console.log(interaction_second.values[0]);
                                         console.log(skills[0]);
                                         console.log(skillSelected);
                                         await interaction_second.reply({ content: `${current_character[0][0].name}'s **${skillSelected.name}**: ${skill.description}` });
