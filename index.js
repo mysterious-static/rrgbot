e0d3aa387134c45a8dab45cd05c056a9ed7d0ccd
aletson <me@ajl.io>
2025-05-05 09:54:47 -0400

    update a different component

--- index.js
+++ index.js
@@ -3802,7 +3802,7 @@ client.on('interactionCreate', async (interaction) => {
                                         }
 
                                         if (typeahead_results[0].length == 0) {
-                                            await interaction_second.update({ content: 'No match was found with the autocomplete text you entered. Please try again.', components: [], ephemeral: true });
+                                            await submittedModal.update({ content: 'No match was found with the autocomplete text you entered. Please try again.', components: [], ephemeral: true });
                                         } else if (typeahead_results[0].length == 1) {
                                             let insertedEffect;
                                             if (type_qty) {
