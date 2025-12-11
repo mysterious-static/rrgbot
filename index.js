cff586df7a15fb4866554c2478c316871b79e712
aletson <me@ajl.io>
2025-12-10 21:56:33 -0500

    make it explicit

--- index.js
+++ index.js
@@ -5903,7 +5903,7 @@ client.on('interactionCreate', async (interaction) => {
                         }
                         valid = 0;
                     }
-                    if (valid) {
+                    if (valid == 1) {
                         console.log('trip');
                         await connection.promise().query('update rps set ?? = ? where id = ?', queryData);
                         rps = await connection.promise().query('select * from rps where id = ?', [rps[0][0].id]);
@@ -5965,9 +5965,10 @@ client.on('interactionCreate', async (interaction) => {
                         valid = 0;
                     }
                 }
-                if (valid) {
+                if (valid == 1) {
                     await connection.promise().query('update rps set ?? = ? where id = ?', queryData);
                     rps = await connection.promise().query('select * from rps where id = ?', rps[0][0].id);
+                    console.log
                     let throwfull = await connection.promise().query('select * from duels_attacks where id = ?', rpsthrow);
                     throwfull = throwfull[0][0].name;
                     if (rps[0][0].challenged_attack_id && rps[0][0].challenger_attack_id) {
